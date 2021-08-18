98bd90dfbf315823000dbfb0108091dcae721e6f
lsglobalinc <hkcho@lsglobalinc.com>
2021-08-18 11:35:46 +0900

    제목: 검색 개발 완료 1 내용: 검색 개발 완료 1       - 검색 개발 완료 1 추가:

--- frontend/vue/src/views/Search.vue
+++ frontend/vue/src/views/Search.vue
@@ -5,29 +5,29 @@
                 <div class="col-lg-8 col-sm-7 py-4">
                     <span class="text-light bg-dark" style="padding: 3px 5px;"> <i class="fa fa-list-ol" aria-hidden="true"></i></span> <strong>{{ $t('Total results') }} : {{ search.total }} </strong>
                 </div>
-                <Order @change="change_order" />
+                <Order @change="change_order"/>
             </div>
             <div class="row">
                 <div class="col small">
                     <div class="card mb-2" v-for="post in search.results" :key="post.id">
                         <div class="card-body">
                             <p class="card-text text-truncate font-weight-bold">
-                                <router-link :to="{ path: '/post/' + post.id }" style="color: #5bc0de"> <i class="fas fa-external-link-alt" aria-hidden="true"></i>{{ post.subject }}</router-link>
+                                <router-link :to="{ path: '/post/' + post.id }" style="color: #5bc0de"><i class="fas fa-external-link-alt" aria-hidden="true"></i>{{ post.subject }}</router-link>
                             </p>
                         </div>
                         <ul class="list-group list-group-flush">
                             <li class="list-group-item img-fluid">
                                 <i class="fa fa-sticky-note" aria-hidden="true"></i>
-                                <div v-html="post.content" />
+                                <div v-html="post.content"/>
                             </li>
                             <li class="list-group-item img-fluid" v-show="post.reply_content">
                                 <i class="fa fa-comment-dots" aria-hidden="true"></i>
-                                <div v-html="post.reply_content" />
+                                <div v-html="post.reply_content"/>
                             </li>
                             <li class="list-group-item  text-light bg-secondary">
                                 <div class="cart-text">
                                     <div>
-                                       <i class="far fa-sticky-note" aria-hidden="true"></i> {{ post.category_name }} <i class="fa fa-user" ></i> {{ post.user_name }} <i class="fa fa-thumbs-up" aria-hidden="true"> </i> {{ post.recommend_count }}
+                                        <i class="far fa-sticky-note" aria-hidden="true"></i> {{ post.category_name }} <i class="fa fa-user"></i> {{ post.user_name }} <i class="fa fa-thumbs-up" aria-hidden="true"> </i> {{ post.recommend_count }}
                                         <i class="fa fa-comment-dots" aria-hidden="true"></i> {{ post.reply_count }} <i class="fa fa-envelope-open" aria-hidden="true"></i> {{ post.click_count }} <i class="fa fa-calendar" aria-hidden="true"> </i> {{ post.local_datetime }}
                                     </div>
                                 </div>
