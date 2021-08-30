5965a1090ff6f3d4270f389f34af52551537cb22
lsglobalinc <hkcho@lsglobalinc.com>
2021-08-30 17:16:25 +0900

    제목: 회원가입 개발 중 5 내용: 회원가입 개발 중 5       - 비밀번호 변경 개발 중 추가:

--- backend/api/user/urls.py
+++ backend/api/user/urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.urls import path, re_path
 
-from backend.api.user.views import Login, IsAuth, Logout, RegisterView, EmailConfirmView, AppIdFindView
+from backend.api.user.views import Login, IsAuth, Logout, RegisterView, EmailConfirmView, AppIdFindView, PasswordResetView, PasswordResetConfirmView, PasswordChangeView
 
 urlpatterns = [
     path('login/', Login.as_view(), name='login'),
@@ -10,5 +10,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     re_path(r'^email/confirm/(?P<key>[-:\w]+)/$', EmailConfirmView.as_view(), name="email_confirm"),
     path('find/app_id/', AppIdFindView.as_view(), name='app_id_find'),
-    path('find/password/', AppIdFindView.as_view(), name='password_find'),
+    path('password/reset/', PasswordResetView.as_view(), name='password_reset'),
+    re_path(r'^password/reset/confirm/(?P<uid>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/(?P<password>.+)/$', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path(r'^password/change/$', PasswordChangeView.as_view(), name='password_change'),
 ]
--- backend/api/user/views.py
+++ backend/api/user/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
-from django.contrib.auth import user_logged_in, authenticate
+import random
+import string
+
+from django.contrib.auth import authenticate
+from django.contrib.auth import user_logged_in
+from django.contrib.auth.tokens import PasswordResetTokenGenerator, default_token_generator
 from django.http import HttpResponse
+from django.utils.encoding import force_text, force_bytes
+from django.utils.http import urlsafe_base64_decode as uid_decoder, urlsafe_base64_encode
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import status
 from rest_framework.exceptions import AuthenticationFailed, PermissionDenied, ParseError
@@ -116,13 +123,68 @@ class PasswordResetView(GenericAPIView, EmailMixin):
         if not user.is_active:
             raise PermissionDenied(detail='User account is disabled.')
 
-        self.send_email(user, 'auth/app_id_find_subject.txt', 'auth/app_id_find_email.html')
+        self.send_email(user, 'auth/password_reset_subject.txt', 'auth/password_reset_email.html')
         return Response('Password reset e-mail has been sent.')
+
+    def get_extras(self, user):
+        return {
+            'password': ''.join(random.choices(string.ascii_letters + string.digits, k=10)),
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
+            'token': PasswordResetTokenGenerator().make_token(user),
+            'host': self.request.get_host()
+        }
 
 
 class PasswordResetConfirmView(GenericAPIView):
     permission_classes = (AllowAny,)
 
-    def get(self, request, **kwargs):
+    def get(self, request, *args, **kwargs):
+        try:
+            uid = force_text(uid_decoder(self.kwargs['uid']))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            raise AuthenticationFailed(detail='user matching query does not exist.')
+        if not default_token_generator.check_token(user, self.kwargs['token']):
+            raise AuthenticationFailed(detail='Invalid token.')
+
+        self.reset_password(user)
         return HttpResponse(_('Password has been reset with the new password.'))
 
+    def reset_password(self, user):
+        password = self.kwargs['password']
+        user.set_password(password)
+        user.save()
+
+
+class PasswordChangeView(GenericAPIView, EmailMixin):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            user = User.objects.get(pk=self.request.user.id)
+        except User.DoesNotExist:
+            raise AuthenticationFailed(detail='User matching query does not exist.')
+
+        if not user.is_active:
+            raise AuthenticationFailed(detail='User account is disabled.')
+
+        if not user.check_password(self.request.data['old_password']):
+            raise AuthenticationFailed(detail='Invalid old password.')
+
+        if self.request.data['new_password1'] != self.request.data['new_password2']:
+            raise AuthenticationFailed(detail="The two password fields didn't match.")
+
+        self.change_password(user)
+        self.send_email(user, 'auth/password_changed_subject.txt', 'auth/password_changed_email.html')
+        return Response('Password has been changed with the new password.')
+
+    def get_extras(self, user):
+        return {
+            'old_password': self.request.data['old_password'],
+            'new_password': self.request.data['new_password1']
+        }
+
+    def change_password(self, user):
+        password = self.request.data["new_password1"]
+        user.set_password(password)
+        user.save()
