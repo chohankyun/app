5a60a35dea9ef907f598cd52c58535a6c52de93c
chohankyun <chohankyun@gmail.com>
2021-09-25 02:54:49 +0900

    제목: 메신저 개발 중 내용: 화면 개발 증     - requirements.txt 추가 추가:

--- backend/drf/settings.py
+++ backend/drf/settings.py
@@ -219,12 +219,12 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/backend/'
 
 if DEBUG:
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'backend'),)
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'backend')
 
 # Auth User
 AUTH_USER_MODEL = 'uauth.User'
