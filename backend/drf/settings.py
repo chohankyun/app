b0ff4c94d2a1472f2628b0e1e40a49799b2cd28d
lsglobalinc <hkcho@lsglobalinc.com>
2021-09-29 18:38:26 +0900

    제목: post table 수정 내용: post reply table 수정       - text_content blank = true 추가:

--- backend/drf/settings.py
+++ backend/drf/settings.py
@@ -218,12 +218,12 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/web/'
 
 if DEBUG:
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'web'),)
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'web')
 
 # Auth User
 AUTH_USER_MODEL = 'uauth.User'
