3354a09345a6eaec6ad10ce71b53a34d5ffd1d71
lsglobalinc <hkcho@lsglobalinc.com>
2021-09-29 19:03:22 +0900

    제목: post table 수정 내용: post reply table 수정       - text_content blank = true 추가:

--- backend/drf/routing.py
+++ backend/drf/routing.py
@@ -7,7 +7,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from backend.api.chat import routing
 
 application = ProtocolTypeRouter({
-    # 'http': get_asgi_application(),
+    'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter(
             routing.websocket_urlpatterns
